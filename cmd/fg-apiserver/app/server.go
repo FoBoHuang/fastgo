c891fa8ae059e91fbc330992a47f32297ca1bab7
fob <hfb161716@163.com>
2025-07-10 00:38:39 +0800

    configFile配置读取、命令行参数设置

--- cmd/fg-apiserver/app/server.go
+++ cmd/fg-apiserver/app/server.go
@@ -1,12 +1,21 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/FoBoHuang/fastgo/cmd/fg-apiserver/app/options"
 )
 
+var configFile string // 配置文件路径
+
 func NewFastGOCommand() *cobra.Command {
+	// 创建默认的应用命令行选项
+	opts := options.NewServerOptions()
+
 	cmd := &cobra.Command{
 		Use:   "fg-apiserver",
 		Short: "A very lightweight full go project",
@@ -14,12 +23,35 @@ func NewFastGOCommand() *cobra.Command {
         learn Go project development.`,
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("Hello FastGO!")
+			// 将 viper 中的配置解析到选项 opts 变量中.
+			if err := viper.Unmarshal(opts); err != nil {
+				return err
+			}
+
+			// 对命令行选项值进行校验.
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+
+			fmt.Printf("Read MySQL host from Viper: %s\n", viper.GetString("mysql.host"))
+			fmt.Printf("Read MySQL username from opts: %s\n", opts.MySQLOptions.Username)
+			fmt.Printf("Read ConfigFile Success.\n")
+
+			jsonData, _ := json.MarshalIndent(opts, "", "  ")
+			fmt.Println(string(jsonData))
+
 			return nil
 		},
 		// 设置命令运行时的参数检查，不需要指定命令行参数。例如：./fg-apiserver param1 param2
 		Args: cobra.NoArgs,
 	}
+
+	// 初始化配置函数，在每个命令运行时调用
+	cobra.OnInitialize(onInitialize)
+
+	// cobra 支持持久性标志(PersistentFlag)，该标志可用于它所分配的命令以及该命令下的每个子命令
+	// 推荐使用配置文件来配置应用，便于管理配置项
+	cmd.PersistentFlags().StringVarP(&configFile, "config", "c", filePath(), "Path to the fg-apiserver configuration file.")
 
 	return cmd
 }
