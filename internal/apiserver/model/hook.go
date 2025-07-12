5a04b66cc58542ad36aa96798c2d0e863df88700
fob <hfb161716@163.com>
2025-07-12 17:48:05 +0800

    fix

--- internal/apiserver/model/hook.go
+++ internal/apiserver/model/hook.go
@@ -7,14 +7,14 @@ import (
 )
 
 // AfterCreate 在创建数据库记录之后生成 postID.
-func (m *PostM) AfterCreate(tx *gorm.DB) error {
+func (m *Post) AfterCreate(tx *gorm.DB) error {
     m.PostID = rid.PostID.New(uint64(m.ID))
 
     return tx.Save(m).Error
 }
 
 // AfterCreate 在创建数据库记录之后生成 userID.
-func (m *UserM) AfterCreate(tx *gorm.DB) error {
+func (m *User) AfterCreate(tx *gorm.DB) error {
     m.UserID = rid.UserID.New(uint64(m.ID))
 
     return tx.Save(m).Error
