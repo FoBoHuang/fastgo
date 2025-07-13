d9d3708eb68c3223d5ad7ed4fb6f84d5a17ecfcb
fob <hfb161716@163.com>
2025-07-14 02:15:55 +0800

    biz层实现

--- internal/apiserver/model/hook.go
+++ internal/apiserver/model/hook.go
@@ -1,21 +1,34 @@
 package model
 
 import (
-    "gorm.io/gorm"
+	"github.com/onexstack/onexstack/pkg/authn"
+	"gorm.io/gorm"
 
-    "github.com/FoBoHuang/fastgo/internal/pkg/rid"
+	"github.com/FoBoHuang/fastgo/internal/pkg/rid"
 )
 
 // AfterCreate 在创建数据库记录之后生成 postID.
 func (m *Post) AfterCreate(tx *gorm.DB) error {
-    m.PostID = rid.PostID.New(uint64(m.ID))
+	m.PostID = rid.PostID.New(uint64(m.ID))
 
-    return tx.Save(m).Error
+	return tx.Save(m).Error
+}
+
+// BeforeCreate 在创建数据库记录之前加密明文密码.
+func (m *User) BeforeCreate(tx *gorm.DB) error {
+	// Encrypt the user password.
+	var err error
+	m.Password, err = authn.Encrypt(m.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // AfterCreate 在创建数据库记录之后生成 userID.
 func (m *User) AfterCreate(tx *gorm.DB) error {
-    m.UserID = rid.UserID.New(uint64(m.ID))
+	m.UserID = rid.UserID.New(uint64(m.ID))
 
-    return tx.Save(m).Error
+	return tx.Save(m).Error
 }
