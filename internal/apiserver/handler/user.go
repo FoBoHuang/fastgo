b075ec2c9817fea01fdd93f3ef182b121ffd1e01
huangfobo <huangfobo@inke.cn>
2025-07-14 11:40:17 +0800

    实现身份认证(JWT)

--- internal/apiserver/handler/user.go
+++ internal/apiserver/handler/user.go
@@ -7,8 +7,71 @@ import (
 
 	"github.com/FoBoHuang/fastgo/internal/pkg/core"
 	"github.com/FoBoHuang/fastgo/internal/pkg/errorsx"
-	"github.com/FoBoHuang/fastgo/pkg/api/apiserver/v1"
+	v1 "github.com/FoBoHuang/fastgo/pkg/api/apiserver/v1"
 )
+
+// Login 用户登录并返回 JWT Token.
+func (h *Handler) Login(c *gin.Context) {
+	slog.Info("Login function called")
+
+	var rq v1.LoginRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, nil, errorsx.ErrBind)
+		return
+	}
+
+	// 小作业：请你自行补全校验代码
+
+	resp, err := h.biz.UserV1().Login(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, nil, err)
+		return
+	}
+
+	core.WriteResponse(c, resp, nil)
+}
+
+// RefreshToken 刷新 JWT Token.
+func (h *Handler) RefreshToken(c *gin.Context) {
+	slog.Info("Refresh token function called")
+
+	var rq v1.RefreshTokenRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, nil, errorsx.ErrBind)
+		return
+	}
+
+	// 小作业：请你自行补全校验代码
+
+	resp, err := h.biz.UserV1().RefreshToken(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, nil, err)
+		return
+	}
+
+	core.WriteResponse(c, resp, nil)
+}
+
+// ChangeUserPassword 修改用户密码.
+func (h *Handler) ChangePassword(c *gin.Context) {
+	slog.Info("Change password function called")
+
+	var rq v1.ChangePasswordRequest
+	if err := c.ShouldBindJSON(&rq); err != nil {
+		core.WriteResponse(c, nil, errorsx.ErrBind)
+		return
+	}
+
+	// 小作业：请你自行补全校验代码
+
+	resp, err := h.biz.UserV1().ChangePassword(c.Request.Context(), &rq)
+	if err != nil {
+		core.WriteResponse(c, nil, err)
+		return
+	}
+
+	core.WriteResponse(c, resp, nil)
+}
 
 // CreateUser 创建新用户.
 func (h *Handler) CreateUser(c *gin.Context) {
---
+++ pkg/token/doc.go
@@ -0,0 +1,1 @@
+package token // import "github.com/FoBoHuang/onexstack/pkg/token"
